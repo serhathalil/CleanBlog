7d079b2cc9e4c11415fe277012462060d9d7d04f
Serhat Halil Silsüpür <serhatsilsupur@gmail.com>
2021-11-19 17:01:01 +0300

    CleanBlog 2.bölüm ödev8

--- app.js
+++ app.js
@@ -1,13 +1,29 @@
 const express = require('express');
+const ejs = require("ejs");
 const app = express();
 
+const blog = { id: 1, title: 'Blog title', description: 'Blog description' };
 
-const blog = { id: 1, title: "Blog title", description: "Blog description" }
-app.get('/',(req,res)=>{
-  res.send("Merhaba")
-})
+//template engine
+app.set("view engine", "ejs");
+//middlewares
+app.use(express.static("public"));
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+app.get('/about', (req, res) => {
+    res.render('about');
+  });
+  app.get('/add_post', (req, res) => {
+    res.render('add_post');
+  });
+  app.get('/post', (req, res) => {
+    res.render('post');
+  });
 const port = 3000;
 
 app.listen(port, () => {
   console.log(`Sunucu ${port} portunda başlatıldı`);
 });
+   
