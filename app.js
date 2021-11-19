2635076da1a83fb1c6874d89e23039fc84b6bf09
Serhat Halil Silsüpür <serhatsilsupur@gmail.com>
2021-11-19 15:12:33 +0300

    yanlış commit

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
 const express = require('express');
 const app = express();
 
-const blog = { id: 1, title: 'Blog title', description: 'Blog description' };
 
-app.get('/', (req, res) => {
-  res.send(blog);
-});
+const blog = { id: 1, title: "Blog title", description: "Blog description" }
+app.get('/',(req,res)=>{
+  res.send("Merhaba")
+})
 const port = 3000;
 
 app.listen(port, () => {
   console.log(`Sunucu ${port} portunda başlatıldı`);
 });
-   
