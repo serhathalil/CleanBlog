0a0fff78d5494274cd83f94d85808e50ea5a425c
Serhat Halil Silsüpür <serhatsilsupur@gmail.com>
2021-11-25 11:54:16 +0300

    Revert "ödev 3"
    This reverts commit 6514d220b080d5bd723b6674f9fdae5a0162eb4e.

--- app.js
+++ app.js
@@ -1,48 +1,29 @@
 const express = require('express');
-const mongoose = require('mongoose');
-const ejs = require('ejs');
-// const path = require('path');
-const Article = require('./models/Article');
+const ejs = require("ejs");
 const app = express();
-// const blog = { id: 1, title: 'Blog title', description: 'Blog description' };
 
-//connect db
-mongoose.connect('mongodb://localhost/cleanblog-test-db', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+const blog = { id: 1, title: 'Blog title', description: 'Blog description' };
 
 //template engine
-app.set('view engine', 'ejs');
-
+app.set("view engine", "ejs");
 //middlewares
-app.use(express.static('public'));
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(express.static("public"));
 
-app.get('/', async (req, res) => {
-  const articles = await Article.find({});
-
-  res.render('index', {
-    articles,
-  });
+app.get('/', (req, res) => {
+  res.render('index');
 });
 app.get('/about', (req, res) => {
-  res.render('about');
-});
-app.get('/add_post', (req, res) => {
-  res.render('add_post');
-});
-app.get('/post', (req, res) => {
-  res.render('post');
-});
-app.post('/articles', async (req, res) => {
-  await Article.create(req.body);
-  res.redirect('/');
-});
-
+    res.render('about');
+  });
+  app.get('/add_post', (req, res) => {
+    res.render('add_post');
+  });
+  app.get('/post', (req, res) => {
+    res.render('post');
+  });
 const port = 3000;
 
 app.listen(port, () => {
   console.log(`Sunucu ${port} portunda başlatıldı`);
 });
+   
